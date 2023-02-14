df767972ac210a093f6b383f8ef8b2a166656d52
Esraa Taha <etaha643@gmail.com>
2023-02-14 20:48:41 +0200

    Add project files.

---
+++ Info_API/Controllers/InfoController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Info_API.Controllers
+{
+    [Route("api/Info")]
+    [ApiController]
+    public class InfoController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Models.Info> GetInfoes()
+        {
+            return new List<Models.Info>
+            {
+                new Models.Info{ Id = 1, First_Name = "Esraa", Last_Name = "Taha"},
+                new Models.Info{ Id = 2,First_Name = "Ahmed", Last_Name = "Taha"}
+            };
+        }
+    }
+}
---
+++ Info_API/Models/Info.cs
@@ -0,0 +1,9 @@
+﻿namespace Info_API.Models
+{
+    public class Info
+    {
+        public int Id { get; set; }
+        public string? First_Name { get; set; }
+        public string? Last_Name { get; set; }
+    }
+}
